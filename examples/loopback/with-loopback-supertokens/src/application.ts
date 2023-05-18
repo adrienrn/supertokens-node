d20260b259ed7c4914cfa579197f78c50f329a3a
Hussard <adrien.ricartnoblet@gmail.com>
2023-05-18 18:09:02 +0200

    Add memory.datasource.ts

--- examples/loopback/with-loopback-supertokens/src/application.ts
+++ examples/loopback/with-loopback-supertokens/src/application.ts
@@ -1,13 +1,14 @@
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
+import {MemoryDataSource} from './datasources';
 import {MySequence} from './sequence';
 
 export {ApplicationConfig};
@@ -29,6 +30,8 @@ export class ExampleApp extends BootMixin(
       path: '/explorer',
     });
     this.component(RestExplorerComponent);
+
+    this.dataSource(MemoryDataSource);
 
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
